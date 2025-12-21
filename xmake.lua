4699f691d20a6abb117293d877d2d7fbba3a4d45
BugKun <490964186@qq.com>
2025-12-21 14:20:40 +0800

    []

--- xmake.lua
+++ xmake.lua
@@ -6,18 +6,13 @@ set_project(PROJECT_NAME)
 
 add_requires("pcre2")
 
-option("musl")
-    set_default(os.getenv("XMAKE_BUILD_MUSL") == "1" or false)
-    set_showmenu(true)
-    set_description("Whether to build musl static version")
-
 target("cli")
     set_basename(PROJECT_NAME)
     set_kind("binary")
     set_languages("c11")
     set_version("2.0.0", {build = "%Y%m%d%H%M"})
 
-    if has_config("musl") then
+    if os.getenv("XMAKE_BUILD_MUSL") == "1" then
         add_cflags("-static", "-fuse-ld=musl")
         add_ldflags("-static", "-fuse-ld=musl")
         add_cxflags("-static", "-fuse-ld=musl")
