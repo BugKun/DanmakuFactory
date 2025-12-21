de4e5174d06eb71cf90655838b67a003bafe2813
BugKun <490964186@qq.com>
2025-12-21 13:41:55 +0800

    []

--- xmake.lua
+++ xmake.lua
@@ -12,6 +12,10 @@ target("cli")
     set_languages("c11")
     set_version("2.0.0", {build = "%Y%m%d%H%M"})
 
+    add_cflags("-static", "-fuse-ld=musl")  -- 静态链接musl-libc
+    add_ldflags("-static", "-fuse-ld=musl")  -- 静态链接musl-libc的链接器标志
+    add_cxflags("-static", "-fuse-ld=musl")  -- 如果你的项目是C++项目，使用add_cxflags
+
     add_packages("pcre2")
     add_files("src/*.c", "src/**/*.c")
 
