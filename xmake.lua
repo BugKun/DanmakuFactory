91861a1bcb9f561a4c0b22c5cad087e10da658e6
BugKun <490964186@qq.com>
2025-12-21 10:47:33 +0800

    test

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,12 @@ add_rules("mode.debug", "mode.release")
 
 set_project(PROJECT_NAME)
 
-add_requires("pcre2")
+-- 针对 musl 平台配置 pcre2 静态链接
+add_requires("pcre2", {
+    config = {
+        shared = not (is_plat("linux") and get_config("toolchain") == "musl")
+    }
+})
 
 target("cli")
     set_basename(PROJECT_NAME)
@@ -15,10 +20,17 @@ target("cli")
     add_packages("pcre2")
     add_files("src/*.c", "src/**/*.c")
 
+    -- musl 平台静态编译配置
+    if is_plat("linux") and get_config("toolchain") == "musl" then
+        set_toolchains("musl")
+        add_cflags("-static")
+        add_ldflags("-static")
+        set_targetdir("build/musl")
+    end
+
     on_package(function (target)
         if is_mode("release") then
             import("scripts.package")
-
             package.package_release(target)
         end
     end)
