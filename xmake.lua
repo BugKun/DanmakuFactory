3d88ef988190f0f60496c4143a368ea01f0cd938
BugKun <490964186@qq.com>
2025-12-27 16:23:15 +0800

    []

--- xmake.lua
+++ xmake.lua
@@ -6,6 +6,11 @@ set_project(PROJECT_NAME)
 
 add_requires("pcre2")
 
+option("musl")
+    set_default(os.getenv("XMAKE_BUILD_MUSL") == "1" or false)
+    set_showmenu(true)
+    set_description("Whether to build musl static version")
+
 target("cli")
     set_basename(PROJECT_NAME)
     set_kind("binary")
